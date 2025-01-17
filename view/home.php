fce45c9d4da6abed1b367219cac66307c6e8642f
Chloé Cilia <chloe.cilia68@gmail.com>
2025-01-18 00:12:33 +0100

    add css

--- view/home.php
+++ view/home.php
@@ -1,4 +1,8 @@
-  
+
+<div class="wrapper">
+<div class="content">
+
+     
         <div class="links-wrapper">
             <div class="link link-featured">Featured</div>
             <div class="link link-popular">Popular</div>
@@ -96,8 +100,23 @@
             </div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
         </div>
-  
-  
+        <div class="posts">
+            <div class="user">
+                <img id="avatar" src="public/avatar/avatar1.jpg" alt="avatar1">
+                <p>nickname</p>
+            </div>
+            <div class="post-links-and-path">
+                    <div class="post-path">
+                        <p class="path">category/topic</p>
+                    </div>
+                    <div class="post-links">
+                        <p><i class="fa-solid fa-square-share-nodes"></i></p>
+                        <p>2 <i class="fa-regular fa-message"></i></p>
+                        <p>1 min ago</p>
+                    </div>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+        </div>
   
     </div>
     
@@ -111,3 +130,5 @@
         <div class="side-bar__container-posts"><p>Cinema</p> <p>Anhting related to cinema</p></div>
         <p><a href="">View more</a></p>
     </section>
+    </div>    
+    </div>   
