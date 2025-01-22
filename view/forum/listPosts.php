d23567251bc048a6f64ecea2fe4c266c47a04898
Chloé Cilia <chloe.cilia68@gmail.com>
2025-01-22 14:30:33 +0100

    add css

--- view/forum/listPosts.php
+++ view/forum/listPosts.php
@@ -12,11 +12,13 @@ foreach($posts as $post ){ ?>
 }
 ?>
 
-<p>New Post:</p>
-<form action="index.php?ctrl=forum&action=createPost&id=<?= $topic->getId()?>" method="post">
-    <label for="content">Title:</label>
-    <br>
-    <textarea  name="content" id="content"> </textarea>
-    <br>
-    <input type="submit" name="submit" value="Submit" id="submit">
-</form>
+<div class="form-wrapper topics__wrapper-form">
+        <form action="index.php?ctrl=forum&action=createPost&id=<?= $topic->getId()?>" method="post">
+            <label for="new">New Post:</label>
+            <br>
+            <textarea  name="content" id="new" rows="10" cols="80"> </textarea>
+            <br>
+            <input type="submit" name="submit" value="Submit" id="submit">
+        </form>
+    </div>
+
